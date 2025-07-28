c733c6af4272d855007440b84f34fc88c4cafeb8
simon <simontran18@gmail.com>
2025-07-27 20:24:46 -0400

    added footer

--- pixel-pantry/src/components/service.tsx
+++ pixel-pantry/src/components/service.tsx
@@ -1,9 +1,9 @@
 //Searching function
 //WORK IN PROGRESS
-export const APIService = async (query) => {
+export const APIService = async (query: string) => {
   const response = await fetch(
     // `https://api.edamam.com/search?app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=20&q=${query}`
-    `https://api.spoonacular.com/recipes/complexSearch?query=${query}`
+    `https://api.spoonacular.com/recipes/complexSearch?query=${query}`,
   );
   const data = await response.json();
   return data.hits;
