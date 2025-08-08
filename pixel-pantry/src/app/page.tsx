0d8e6fbd0db537a176dc8a904e6da0065d45623c
Hazen8988 <patrickwildgen@gmail.com>
2025-08-08 14:48:50 -0400

    wallpaper update

--- pixel-pantry/src/app/page.tsx
+++ pixel-pantry/src/app/page.tsx
@@ -6,7 +6,7 @@ import SearchBar from "@/components/searchBar";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-green-50 to-green-100">
+    <div className="min-h-screen wallpaper-bg">
       <div className="container mx-auto px-4 pt-8 pb-4">
         <SearchBar size="lg" className="mb-8" />
       </div>
--- pixel-pantry/src/components/landingPage.tsx
+++ pixel-pantry/src/components/landingPage.tsx
@@ -8,10 +8,10 @@ export default function LandingPage() {
         <section>
             <div className='flex justify-center items-center  wallpaper-bg overflow-hidden min-h-screen relative'>
                 <div
-                    className='w-full flex justify-center items-center px-8 bg-white/80 px-10 py-10 relative z-10 wallpaper-bg'
+                    className='w-full flex justify-center items-center px-8 px-10 py-10 relative z-10'
                 >
                     {/* left side hero text */}
-                    <div className='flex flex-col justify-center max-w-md mr-10 bg-amber-900 backdrop-blur-sm p-6 rounded-xl w-[1200px] h-[400px]' style={{ marginRight: '-100px', zIndex: 20 }}>
+                    <div className='flex flex-col justify-center max-w-md mr-10 bg-amber-900/95 backdrop-blur-sm p-6 rounded-xl w-[1200px] h-[400px]' style={{ marginRight: '-100px', zIndex: 20 }}>
                         <h1 className='text-4xl font-bold mb-4 text-white'>Welcome to <span className='text-yellow-300'>Pixel Pantry</span></h1>
                         <p className='text-lg mr-4 mb-6 text-white'>Join us to find the <span className='text-yellow-300 font-bold'>best</span> recipes for your next meal</p>
                         <button className='bg-yellow-300 text-white px-6 py-3 rounded-md shadow-lg hover:bg-yellow-600 transition-all duration-300 w-fit'>Use our recpie finder</button>
